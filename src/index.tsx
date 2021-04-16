5a4466b612c2464e9788bb46ea2edd85929f676a
SplinterDev <fabriciojcmontenegro@gmail.com>
2021-04-15 23:22:36 -0300

    Add footer

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,17 @@ import './index.scss'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <footer>
+      <span>
+        &copy; <a href="https://github.com/SplinterDev">@SplinterDev</a> 2021
+      </span>
+      <a href="https://github.com/SplinterDev/todo/blob/master/LICENSE">
+        Terms of Service
+      </a>
+      <a href="https://github.com/SplinterDev/todo">
+        This repository on GitHub
+      </a>
+    </footer>
   </React.StrictMode>,
   document.getElementById('root'),
 )
