6401461d83f658ec08ccb8954435591b86d320c8
SplinterDev <fabriciojcmontenegro@gmail.com>
2021-04-17 11:45:43 -0300

    Create Header

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import App from '../App/App'
+import Header from '../Header/Header'
 import './Body.scss'
 
 const Body = (): JSX.Element => {
   return (
     <div className="Body">
+      <Header />
       <App />
     </div>
   )
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = (): JSX.Element => {
+  return <div className="Header">Header</div>
+}
+
+export default Header
