9febbf3e3b9b61058d71cc9a59faab332722f3ad
SplinterDev <fabriciojcmontenegro@gmail.com>
2021-04-17 12:39:55 -0300

    Add header menu buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Clock from '../Clock/Clock'
 import './Header.scss'
 import HeaderMenu from './HeaderMenu'
 
 const Header = (): JSX.Element => {
+  const [label, setLabel] = useState<string | null>(null)
+
   return (
     <div className="Header">
       <h1>To Be Done</h1>
       <aside>
-        <Clock />
-        <HeaderMenu />
+        {label ? <div className="label">{label}</div> : <Clock />}
+        <HeaderMenu onChangeLabel={setLabel} />
       </aside>
     </div>
   )
--- src/components/Header/HeaderMenu.tsx
+++ src/components/Header/HeaderMenu.tsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HeaderMenu.scss'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
-import { faQuestion, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCheck,
+  faCheckDouble,
+  faQuestion,
+  faTimes,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons'
 
-const HeaderMenu = (): JSX.Element => {
-  return (
+type Props = {
+  onChangeLabel: (label: string | null) => void
+}
+
+const HeaderMenu = ({ onChangeLabel }: Props): JSX.Element => {
+  const [isConfirm, setIsConfirm] = useState(false)
+
+  const clearLabel = () => {
+    onChangeLabel(null)
+  }
+
+  return !isConfirm ? (
     <div className="HeaderMenu">
-      <Icon className="menu-icon" icon={faQuestion} />
-      <Icon className="menu-icon" icon={faTrashAlt} />
+      <Icon
+        className="menu-icon"
+        icon={faQuestion}
+        onMouseEnter={() => onChangeLabel('Help')}
+        onMouseLeave={clearLabel}
+      />
+      <Icon
+        className="menu-icon"
+        icon={faTrashAlt}
+        onClick={() => setIsConfirm(true)}
+        onMouseEnter={() => onChangeLabel('Delete all')}
+        onMouseLeave={clearLabel}
+      />
+    </div>
+  ) : (
+    <div className="HeaderMenu">
+      <Icon
+        className="menu-icon"
+        icon={faTimes}
+        onMouseEnter={() => onChangeLabel('Cancel')}
+        onMouseLeave={clearLabel}
+      />
+      <Icon
+        className="menu-icon"
+        icon={faCheck}
+        onMouseEnter={() => onChangeLabel('Delete checked')}
+        onMouseLeave={clearLabel}
+      />
+      <Icon
+        className="menu-icon"
+        icon={faCheckDouble}
+        onMouseEnter={() => onChangeLabel('Delete all')}
+        onMouseLeave={clearLabel}
+      />
     </div>
   )
 }
